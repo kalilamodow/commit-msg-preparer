18dcf238e56ef91930aff1e5e4d74a0d3e5f6eba
Kalilamodow <kalilamodow@gmail.com>
2026-08-12 13:09:15 -0500

    fix embedded manifest

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@
 // makes it look modern
 #pragma comment(linker, "\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
-processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='EN'\"")
+processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #include <wchar.h>
 
