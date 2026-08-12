e407a0202b0008794aa18c9f5051c58825eed854
Kalilamodow <kalilamodow@gmail.com>
2026-08-12 01:02:14 -0500

    fix manifest embedding

--- release-build.cmd
+++ release-build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-cl /Gy /Gw /O1 src\main.c /GS- /link kernel32.lib user32.lib gdi32.lib /ENTRY:CustomEntry /SUBSYSTEM:CONSOLE /NODEFAULTLIB /OPT:REF /OPT:ICF /INCREMENTAL:NO /DEBUG:NONE /ALIGN:16 /NOCOFFGRPINFO /out:target.exe
+cl /Gy /Gw /O1 src\main.c /GS- /link kernel32.lib user32.lib gdi32.lib /ENTRY:CustomEntry /SUBSYSTEM:CONSOLE /NODEFAULTLIB /OPT:REF /OPT:ICF /INCREMENTAL:NO /DEBUG:NONE /ALIGN:16 /NOCOFFGRPINFO /MANIFEST:EMBED /out:target.exe
 
 echo.
 echo.
